40a011cabebab7ac816611366158112ae526461a
Keen Corsiga <keencorsiga@yahoo.com>
2020-07-11 16:49:02 +0800

    Add User, edit and update

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Successfully created user"
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Account updated successfully"
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
+
+  end
+
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
